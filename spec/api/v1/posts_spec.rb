64b15ce4b21e0bd17ee6305cf6b7a523d841f157
Nikolay Norkin <n.norkin@rambler-co.ru>
2015-11-22 00:32:24 +0300

    Update and delete post specs

--- spec/api/v1/posts_spec.rb
+++ spec/api/v1/posts_spec.rb
@@ -153,10 +153,162 @@ describe 'Profile', type: :api do
   end
 
   describe 'PUT /posts/:id' do
+    subject { last_response }
 
+    context 'when not authenticated' do
+      before do
+        put '/v1/posts/123'
+      end
+
+      its(:status) { is_expected.to eq(401) }
+    end
+
+    context 'when authenticated' do
+      let(:params) { {} }
+      before do
+        authenticate_user user
+        put "/v1/posts/#{id}",
+          params.to_json,
+          'CONTENT_TYPE' => 'application/json'
+      end
+
+      context 'and post exist' do
+        let(:id) { post.id }
+
+        context 'and post belongs to user' do
+          let(:post) { create(:post, user: user) }
+
+          context 'and body present' do
+            let(:body) { 'asddas' }
+            let(:params) { { body: body } }
+
+            it 'updates body and renders updated post' do
+              expect(last_response.status).to eq(200)
+              expect(post.reload.body).to eq(body)
+              expect(json[:post][:id]).to eq(post.id)
+              expect(json[:post][:body]).to eq(body)
+            end
+          end
+
+          context 'but no body present' do
+            let(:params) { {} }
+
+            it 'renders error' do
+              expect(last_response.status).to eq(500)
+              expect(json[:status]).to eq('error')
+              expect(json[:message]).to match(/body is missing/)
+            end
+          end
+        end
+
+        context 'but post belongs to different user' do
+          let(:post) { create(:post) }
+          let(:params) { { body: 'asdsa' } }
+
+          it 'renders error' do
+            expect(last_response.status).to eq(403)
+            expect(json[:status]).to eq('error')
+            expect(json[:message]).to match(
+              I18n.t('api.errors.you_dont_have_access')
+            )
+          end
+        end
+      end
+
+      context 'but post do not exist' do
+        let(:id) { '4bc94e56-ff6c-4f3c-a867-08a19e5f2e4a' }
+        let(:params) { { body: 'asdsa' } }
+
+        it 'renders error' do
+          expect(last_response.status).to eq(404)
+          expect(json[:status]).to eq('error')
+          expect(json[:message]).to eq(I18n.t('api.errors.not_found'))
+        end
+      end
+
+      context 'but id has wrong format' do
+        let(:id) { '666' }
+        let(:params) { { body: 'asdsa' } }
+
+        it 'renders error' do
+          expect(last_response.status).to eq(500)
+          expect(json[:status]).to eq('error')
+          expect(json[:message]).to match(
+            I18n.t('api.errors.wrong_uuid_format')
+          )
+        end
+      end
+    end
   end
 
   describe 'DELETE /posts/:id' do
+    subject { last_response }
 
+    context 'when not authenticated' do
+      before do
+        delete '/v1/posts/123'
+      end
+
+      its(:status) { is_expected.to eq(401) }
+    end
+
+    context 'when authenticated' do
+      let(:params) { {} }
+      before do
+        authenticate_user user
+        delete "/v1/posts/#{id}",
+          params.to_json,
+          'CONTENT_TYPE' => 'application/json'
+      end
+
+      context 'and post exist' do
+        let(:id) { post.id }
+
+        context 'and post belongs to user' do
+          let(:post) { create(:post, user: user) }
+
+          it 'deletes post and renders message that deletion succeeded' do
+            expect(last_response.status).to eq(200)
+            expect(json).to eq(
+              status: 'ok', message: I18n.t('api.messages.deleted')
+            )
+          end
+        end
+
+        context 'but post belongs to different user' do
+          let(:post) { create(:post) }
+
+          it 'renders error' do
+            expect(last_response.status).to eq(403)
+            expect(json[:status]).to eq('error')
+            expect(json[:message]).to match(
+              I18n.t('api.errors.you_dont_have_access')
+            )
+          end
+        end
+      end
+
+      context 'but post do not exist' do
+        let(:id) { '4bc94e56-ff6c-4f3c-a867-08a19e5f2e4a' }
+
+        it 'renders error' do
+          expect(last_response.status).to eq(404)
+          expect(json[:status]).to eq('error')
+          expect(json[:message]).to eq(I18n.t('api.errors.not_found'))
+        end
+      end
+
+      context 'but id has wrong format' do
+        let(:id) { '666' }
+
+        it 'renders error' do
+          expect(last_response.status).to eq(500)
+          expect(json[:status]).to eq('error')
+          expect(json[:message]).to match(
+            I18n.t('api.errors.wrong_uuid_format')
+          )
+        end
+      end
+    end
   end
 end
